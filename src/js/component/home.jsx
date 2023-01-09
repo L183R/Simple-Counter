4d32c173414b7220c12532b50c24d7b03252a6d8
L183R <liberolascuaga@gmail.com>
2023-01-09 15:21:59 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,21 @@
 import React from "react";
+import Navbar from "./navbar.jsx"; // <Navbar titulo="Perrito"/> ==> Navbar("Perrito")
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="bg-dark text-warning p-3 d-flex">
+				<img src="https://www.unila.edu.mx/wp-content/uploads/2022/02/que-hace-un-contador1-2048x1365.jpg" className="m-1" style={{width: "60px"}}></img>
+				<h1>Contador: {props.numero6}{props.numero5}{props.numero4}{props.numero3}{props.numero2}{props.numero1}</h1>
+			</div>
+
+		</>
 	);
 };
 
